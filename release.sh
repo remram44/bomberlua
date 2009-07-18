bd940dc734eb72e799b2cbe91fa844dce8295507
remram44 <remirampin@gmail.com>
2009-07-18 16:50:53 +0200

    Mise à jour script release.sh

--- release.sh
+++ release.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
+# Identifieur de la release
+REL=$(date +"%Y%m%d")
+echo "Packaging release $REL..."
+echo "Packaging release $REL..." > release.log
+
 # Fichier ZIP avec sources + binaires Windows (.exe et .dll)
-rm -f bomberlua.zip
-find . -not \( -type d -or -wholename './.hg*' -or -name '*.o' -or -name '*.zip' -or -name '*.tar.gz' -or -wholename './launcher/Makefile*' -or -wholename './launcher/release/*' \) | zip -q bomberlua.zip -@ 2>&1 > release.log
+rm -f bomberlua-$REL.zip
+find . -not \( -type d -or -wholename './.hg*' -or -name '*.o' -or -name '*.zip' -or -name '*.tar.gz' -or -name '*.log' -or -wholename './launcher/Makefile*' -or -wholename './launcher/release/*' \) | zip -q bomberlua-$REL.zip -@ 2>&1 >> release.log
 
 # Fichier TGZ avec sources et media uniquement
 cd ..
-rm -f bomberlua/bomberlua.tar.gz
-find bomberlua -not \( -type d -or -wholename 'bomberlua/.hg*' -or -name '*.o' -or -name '*.zip' -or -name '*.tar.gz' -or -wholename 'bomberlua/launcher/Makefile*' -or -wholename 'bomberlua/launcher/release/*' -or -name '*.exe' -or -name '*.dll' \) | xargs tar zcf bomberlua.tar.gz 2>&1 >> release.log
-mv bomberlua.tar.gz bomberlua/bomberlua.tar.gz
+rm -f bomberlua/bomberlua-$REL.tar.gz
+find bomberlua -not \( -type d -or -wholename 'bomberlua/.hg*' -or -name '*.o' -or -name '*.zip' -or -name '*.tar.gz' -or -name '*.log' -or -wholename 'bomberlua/launcher/Makefile*' -or -wholename 'bomberlua/launcher/release/*' -or -name '*.exe' -or -name '*.dll' \) | xargs tar zcf bomberlua-$REL.tar.gz 2>&1 >> bomberlua/release.log
+mv bomberlua-$REL.tar.gz bomberlua/bomberlua-$REL.tar.gz
 cd bomberlua
